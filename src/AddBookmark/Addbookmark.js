ac74f4ffd18216e3ca7b1744cde793e5a094e0c9
Felj Abas <felabas41@gmail.com>
2020-12-07 15:15:04 -0800

    added new files

---
+++ src/AddBookmark/Addbookmark.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+
+//In React, the text to be displayed in a textarea is added as a value prop, instead as of adding as children
+class Addboomark extends Component {
+    render(){
+        return(
+            <div className='addBookmark'>
+                <h2>Add Bookmark</h2>
+                <form classsName='addbookmark_form'>
+                    <label htmlFor='title'>Title</label>
+                    <input type='text' name='title' id='title' placeholder='title'/>
+                    <label htmlFor='url'>URL</label>
+                    <input type='text' name='url' id='url' placeholder='url'/>
+                    <label htmlFor='description'>Description</label>
+                    <textarea name='description' id='description' placeholder='description'/>
+                    <label htmlFor='rating'>Rating</label>
+                    <input
+                        type='number'
+                        name='rating'
+                        id='rating'
+                        min='1'
+                        max='5'/>
+                    <div className='addbookmark_buttons'>
+                        <button>Cancel</button>
+                        <button type='submit'>Save</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Addboomark;
